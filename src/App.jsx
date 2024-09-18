7546826568a2cca6bf287fe14ca990b82dc97bae
Brianna Dunfield <bdunfield@mta.ca>
2024-09-18 13:11:11 -0300

    Footer element

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.scss'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import NavBar from './components/NavBar/NavBar.jsx';
 import Home from './pages/Home/Home.jsx'
+import Footer from './components/Footer/Footer.jsx';
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home/>}/>
       </Routes>
+      <Footer/>
       </BrowserRouter>
     </>
   )
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,7 @@
+import './Footer.scss';
+
+export default function Footer(){
+    return <footer>
+        <p className='footer'>© Created by Brianna Dunfield, 2024</p>
+    </footer>
+}
