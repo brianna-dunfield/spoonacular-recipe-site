95bec4759abe25ca024589238d0bae19e2fc1aaa
Brianna Dunfield <bdunfield@mta.ca>
2024-09-18 18:31:19 -0300

    pagination functioning

--- src/components/RecipeList/RecipeList.jsx
+++ src/components/RecipeList/RecipeList.jsx
@@ -2,14 +2,13 @@ import './RecipeList.scss';
 import RecipeCard from '../RecipeCard/RecipeCard';
 import { useEffect, useState } from 'react';
 import { getSearchRequest } from '../../utils/api';
+import ReactPaginate from 'react-paginate';
 
 export default function RecipeList({ searchInput }) {
 	const [recipeOptions, setRecipeOptions] = useState([]);
-	const name = 'Broccolini Quinoa Pilaf';
-	const cuisines = ['Mediterranean', 'Italian', 'European'];
-	const time = '30';
-	const servings = '2';
-	const img = 'https://img.spoonacular.com/recipes/715769-312x231.jpg';
+	const [page, setPage] = useState(0);
+	const [filteredRecipes, setFilteredRecipes] = useState([]);
+	const numPerPage = 5;
 
 	useEffect(() => {
 		const fetchRecipes = async () => {
@@ -24,13 +23,21 @@ export default function RecipeList({ searchInput }) {
 		fetchRecipes();
 	}, [searchInput]);
 
+	useEffect(() => {
+		setFilteredRecipes(
+			recipeOptions.filter((item, index) => {
+				return (index >= page * numPerPage) & (index < (page + 1) * numPerPage);
+			})
+		);
+	}, [page, recipeOptions]);
+
 	function formatCuisines(cuisinesArr) {
 		console.log(cuisinesArr);
 		return cuisinesArr.join(', ');
 	}
 	return (
 		<section className='recipes-list'>
-			{recipeOptions.map((recipe, index) => {
+			{filteredRecipes.map((recipe, index) => {
 				return (
 					<RecipeCard
 						key={index}
@@ -42,6 +49,11 @@ export default function RecipeList({ searchInput }) {
 					/>
 				);
 			})}
+			<ReactPaginate
+				onPageChange={(event) => setPage(event.selected)}
+				pageCount={Math.ceil(recipeOptions.length / numPerPage)}
+				breakLabel="..."
+			/>
 		</section>
 	);
 }
