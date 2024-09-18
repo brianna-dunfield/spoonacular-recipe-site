dd5b770e911d6784f076fb6c8a78fb1f13a2afb3
Brianna Dunfield <bdunfield@mta.ca>
2024-09-18 12:57:41 -0300

    Nav bar styling for mobile breakpoint

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,11 +4,21 @@ import { Link } from 'react-router-dom';
 export default function NavBar() {
 	return (
 		<nav className='nav'>
-			<Link to='/'>RECIPE ROVER</Link>
-			<input placeholder='Search' />
-			<div>
-				<button>Search</button>
-				<button>Filter</button>
+			<Link
+				to='/'
+				className='nav__logo'
+			>
+				RECIPE ROVER
+			</Link>
+			<div className='nav__search'>
+				<input
+					placeholder='Search'
+					className='nav__search-input'
+				/>
+				<div className='nav__buttons'>
+					<button className='nav__buttons-search'>Search</button>
+					<button className='nav__buttons-filter'>Filter</button>
+				</div>
 			</div>
 		</nav>
 	);
