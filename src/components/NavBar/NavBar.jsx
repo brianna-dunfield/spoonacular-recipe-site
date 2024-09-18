2075c1847844dbf42ffa84cbdf3e17cd1f5267a6
Brianna Dunfield <bdunfield@mta.ca>
2024-09-18 16:36:53 -0300

    able to get input and set to useState var

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,7 +1,26 @@
 import './NavBar.scss';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
-export default function NavBar() {
+export default function NavBar({ setSearchInput }) {
+	const [currInput, setCurrInput] = useState('');
+
+	const handleBtnClick = () => {
+		setSearchInput(currInput);
+		setCurrInput('');
+	};
+
+	const handleKeyDown = (event) => {
+		if (event.key === 'Enter') {
+			setSearchInput(currInput);
+			setCurrInput('');
+		}
+	};
+
+	const handleInput = (event) => {
+		setCurrInput(event.target.value);
+	};
+
 	return (
 		<nav className='nav'>
 			<Link
@@ -12,11 +31,20 @@ export default function NavBar() {
 			</Link>
 			<div className='nav__search'>
 				<input
+					type='text'
+					value={currInput}
+					onKeyDown={handleKeyDown}
 					placeholder='Search'
+					onChange={handleInput}
 					className='nav__search-input'
 				/>
 				<div className='nav__buttons'>
-					<button className='nav__buttons-search'>Search</button>
+					<button
+						className='nav__buttons-search'
+						onClick={handleBtnClick}
+					>
+						Search
+					</button>
 					<button className='nav__buttons-filter'>Filter</button>
 				</div>
 			</div>
